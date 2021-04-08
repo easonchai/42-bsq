60a4a15727da99d26a9b70dccb62f14f207a7583
jiaaren <jiaren.kjr@gmail.com>
2021-04-08 16:57:59 +0800

    working validation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jkhong <jkhong@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 14:14:42 by jkhong            #+#    #+#             */
-/*   Updated: 2021/04/08 15:04:53 by jkhong           ###   ########.fr       */
+/*   Updated: 2021/04/08 16:56:37 by jkhong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,62 @@ int		get_len(t_init *init, int file)
 	init->width = grid_width;
 	return (init_len);
 }
+int		valid_input(char *init, int len) // return number
+{
+	int i;
+	int tmp;
+	int num;
+	
+	num = 0;
+	if (len < 4)
+		return (-1);
+	i = 0;
+	while (i < len)
+	{
+		if (!(init[i] >= ' ' && init[i] <= '~'))
+			return (-1);
+		tmp = i + 1;
+		while (i >= (len + 3)  && tmp < len)
+		{
+			if (init[tmp] == init[i])
+				return (-1);
+			tmp++;
+		}
+		if (i < (len - 3))
+		{	
+			if (!(init[i] >= '0' && init[i] <= '9'))
+				return (-1);
+			num = (num * 10) + (init[i] - '0');
+		}
+		i++;
+	}
+	return (num);
+}
+
+int		test_input(t_init *init, t_charset *charset, int len, int file)
+{
+	char	buffer[len];
+	char	c;
+	int		i;
+	int		height;
+	
+	i = 0;
+	while (read(file, &c, 1))
+	{
+		if (c == '\n')
+			break;
+		buffer[i] = c;
+		i++;
+	}
+	height = valid_input(buffer, len);
+	if (height == -1)
+		return (0);
+	init->height = height;
+	charset->empty = buffer[len - 3];
+	charset->obstacle = buffer[len - 2];
+	charset->full = buffer[len - 1];
+	return (1);
+}
 
 int		main(int argc, char *argv[])
 {
@@ -55,6 +111,7 @@ int		main(int argc, char *argv[])
 	int			file;
 	t_init		init;
 	t_charset	charset;
+	int			init_len;
 
 	i = 1;
 	while (i < argc) // loop through all of the files
@@ -63,12 +120,24 @@ int		main(int argc, char *argv[])
 		if (file == -1)
 			print_err();
 		else
-		{
-			printf("%i\n", get_len(&init, file));
-			printf("%i\n", init.width);
+		{	
+			init_len = get_len(&init, file);
+			file = open(argv[i], O_RDONLY);
+			if (!test_input(&init, &charset, init_len, file))
+			{
+				close(file);
+				print_err();
+			}
+			else
+			{
+				printf("%i\n", init.height);
+				printf("%i\n", init.width);
+				printf("%c\n", charset.empty);
+				printf("%c\n", charset.obstacle);
+				printf("%c\n", charset.full);
+			}
 		}
 		// find charset (pass in init and charset addresses)
 		i++;
 	}
-
 }
